109cbbe2efdbff386cc41a57d3f1aa2bd99ada9e
automaticaddison <38732101+automaticaddison@users.noreply.github.com>
2018-07-19 12:13:25 -0700

    Initial commit

---
+++ hello_world.py
@@ -0,0 +1,32 @@
+################################################################################
+#   @file           hello_world_py
+#   @author         Addison Sears-Collins
+#   @date           7/19/2018
+#   @version        1.0
+#
+#   @brief	    My first program in Python
+#
+#   Purpose: This program is my first program in Python
+################################################################################
+print("Hello, world")
+print(1 + 2)
+print(7 * 6)
+print()
+print("The End")
+print("hello" + " world")
+greeting = "hello"
+name = "Addison"
+print(greeting + name)
+# if we want a space, we can add that too
+print(greeting + " " + name)
+
+greeting = "Hello"
+name = input("Please enter your name ")
+print(greeting + " " + name + "\n")
+
+split_string = "This string has been\nsplit over\nseveral\nlines"
+print(split_string)
+
+tabbed_string = "1\t2\t3\t4\t5\t"
+print("\n" + tabbed_string)
+
